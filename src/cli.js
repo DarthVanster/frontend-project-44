1c1bcae30004c294feca1be2f470f62dc1e320ba
DarthVanster <alex14fs@mail.ru>
2023-12-07 11:29:08 +0400

    fixedBrainGames3

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
 import readlineSync from 'readline-sync';
 
 export const getAnswerForQuestion = (question) => readlineSync.question(`${question} `);
-export const getUserName = () => getAnswerForQuestion('May i have your name?');
+export const getUserName = () => getAnswerForQuestion('May I have your name?');
