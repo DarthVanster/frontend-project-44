c5f00dee5b2b73684459a47cb36c953fc73a7a68
DarthVanster <147305069+DarthVanster@users.noreply.github.com>
2023-12-10 10:06:05 +0400

    Update calc-game.js

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,1 +1,44 @@
+import getRandomNumber from '../randomNumber.js';
+import { getAnswerForQuestion } from '../answerForQuestion.js';
 
+const operatorsCounts = 3;
+
+const getRandomOperator = () => {
+  switch (getRandomNumber(1, operatorsCounts)) {
+    case 1:
+      return '+';
+    case 2:
+      return '-';
+    case 3:
+      return '*';
+    default:
+      return '+';
+  }
+};
+
+const calculateExpresion = (numberLeft, operator, numberRigth) => {
+  switch (operator) {
+    case '+':
+      return (Number.parseInt(numberLeft, 10) + Number.parseInt(numberRigth, 10)).toString(10);
+    case '-':
+      return (Number.parseInt(numberLeft, 10) - Number.parseInt(numberRigth, 10)).toString(10);
+    case '*':
+      return (Number.parseInt(numberLeft, 10) * Number.parseInt(numberRigth, 10)).toString(10);
+    default:
+      return undefined;
+  }
+};
+
+const taskGame = 'What is the result of the expression?';
+
+const maxRangeNumbers = 30;
+
+const getQuestionAndRigthAnswer = () => {
+  const numberLeft = getRandomNumber(0, maxRangeNumbers);
+  const numberRigth = getRandomNumber(0, maxRangeNumbers);
+  const operator = getRandomOperator();
+  const rigthAnswer = calculateExpresion(numberLeft, operator, numberRigth);
+  return [`${numberLeft} ${operator} ${numberRigth}`, rigthAnswer];
+};
+
+export default game;
