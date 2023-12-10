830eca7ecb8a2c8c60c042d30e2938a02dc6f6f6
DarthVanster <147305069+DarthVanster@users.noreply.github.com>
2023-12-10 09:51:41 +0400

    Update calc-game.js

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -1,26 +1,44 @@
-import { getAnswerForQuestion } from '../answerForQuestion.js';
 import getRandomNumber from '../randomNumber.js';
+import { getAnswerForQuestion } from '../answerForQuestion.js';
 
-const game = (rules) => {
-  const Operations = ['+', '-', '*'];
-  const getOperation = () => {
-    const getOperat = Operations[getRandomNumber(2)];
-    return getOperat;
-  };
-  const next = () => {
-    const num1 = getRandomNumber(99);
-    const num2 = getRandomNumber(99);
-    const operation = getOperation();
-    const rightAnswer = `${num1} ${operation} ${num2}`;
-    console.log(`Question: ${rightAnswer}`);
-    const answer = getAnswerForQuestion('Your answer:');
-    rules.check({ rightAnswer, answer }, next);
-  };
-  return {
-    start() {
-      console.log('What is the result of the expression?');
-      next();
-    },
-  };
+const operatorsCounts = 3;
+
+const getRandomOperator = () => {
+  switch (getRandomNumber(1, operatorsCounts)) {
+    case 1:
+      return '+';
+    case 2:
+      return '-';
+    case 3:
+      return '*';
+    default:
+      return '+';
+  }
 };
+
+const calculateExpresion = (numberLeft, operator, numberRigth) => {
+  switch (operator) {
+    case '+':
+      return (Number.parseInt(numberLeft, 10) + Number.parseInt(numberRigth, 10)).toString(10);
+    case '-':
+      return (Number.parseInt(numberLeft, 10) - Number.parseInt(numberRigth, 10)).toString(10);
+    case '*':
+      return (Number.parseInt(numberLeft, 10) * Number.parseInt(numberRigth, 10)).toString(10);
+    default:
+      return undefined;
+  }
+};
+
+const taskGame = 'What is the result of the expression?';
+
+const maxRangeNumbers = 30;
+
+const getQuestionAndRigthAnswer = () => {
+  const numberLeft = getRandomNumber(0, maxRangeNumbers);
+  const numberRigth = getRandomNumber(0, maxRangeNumbers);
+  const operator = getRandomOperator();
+  const rigthAnswer = calculateExpresion(numberLeft, operator, numberRigth);
+  return [`${numberLeft} ${operator} ${numberRigth}`, rigthAnswer];
+};
+
 export default game;
