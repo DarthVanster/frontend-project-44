38c59b7eb60905d5cc5d44edca25a3434cc1589a
DarthVanster <147305069+DarthVanster@users.noreply.github.com>
2023-12-12 20:51:52 +0400

    Update randomNumber.js

--- src/randomNumber.js
+++ src/randomNumber.js
@@ -1,2 +1,6 @@
-const getRandomNumber = (base) => Math.round(Math.random() * base);
-export default getRandomNumber;
+const getRandomIntInclusive = (min, max) => {
+  const minimal = Math.ceil(min);
+  const maximum = Math.floor(max);
+  return Math.floor(Math.random() * (maximum - minimal + 1) + minimal);
+};
+export default getRandomIntInclusive;
