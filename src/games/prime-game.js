7651446e6b626f9624d6d2673de0a981427d94ef
DarthVanster <147305069+DarthVanster@users.noreply.github.com>
2023-12-12 16:40:09 +0400

    Update prime-game.js

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,28 +1,27 @@
-import { getAnswerForQuestion } from '../answerForQuestion.js';
 import getRandomNumber from '../randomNumber.js';
+import executeGame from '../questionAnswer.js';
 
-const game = (rules) => {
-  const next = () => {
-    const isPrime = (num) => {
-      if (num === 1) return false;
-      if (num % 2 === 0 && num > 2) return false;
-      const s = Math.sqrt(num);
-      for (let i = 3; i <= s; i += 1) {
-        if (num % i === 0) return false;
-      }
-      return true;
-    };
-    const num = getRandomNumber(7) + 2;
-    const rightAnswer = isPrime(num) ? 'yes' : 'no';
-    console.log(`Question: ${num}`);
-    const answer = getAnswerForQuestion('Your answer:');
-    rules.check({ rightAnswer, answer }, next);
-  };
-  return {
-    start() {
-      console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-      next();
-    },
-  };
+const isPrimeNumber = (number) => {
+  if (number < 2) return false;
+  const maxDivisor = Math.floor(Math.sqrt(number));
+  for (let i = 2; i <= maxDivisor; i += 1) {
+    if (number % i === 0) return false;
+  }
+  return true;
 };
-export default game;
+
+const maxRangePrimeNumbers = 100;
+
+const getQuestionAndRigthAnswer = () => {
+  const questionPrimeNumber = getRandomNumber(2, maxRangePrimeNumbers);
+  const rigthAnswer = isPrimeNumber(questionPrimeNumber) ? 'yes' : 'no';
+  return [`${questionPrimeNumber}`, rigthAnswer];
+};
+
+const taskGame = 'Is this number prime?';
+
+const startPrimeGame = () => {
+  executeGame(taskGame, getQuestionAndRigthAnswer);
+};
+
+export default startPrimeGame;
