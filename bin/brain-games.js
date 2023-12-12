df6046729393d717874c69cf8507c3fd17ef5009
DarthVanster <147305069+DarthVanster@users.noreply.github.com>
2023-12-12 16:45:20 +0400

    Update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
+import greeting from '../src/greeting.js';
 
-import { getUserName } from '../src/cli.js';
-
-const startGame = () => {
-  const userName = getUserName();
-  console.log(`Hello, ${userName}!`);
-  console.log('Welcome to the Brain Games!');
-};
-startGame();
+greeting();
