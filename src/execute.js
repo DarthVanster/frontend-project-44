637b9eff373891ab931b7cc01bc98cbfa5e17e33
Alexey <alex14fs@mail.ru>
2023-12-15 18:46:21 +0400

    test4

--- src/execute.js
+++ src/execute.js
@@ -4,9 +4,9 @@ const answersCount = 3;
 
 const braingame = (taskGame, questionAndARigthAnswerGame) => {
   console.log('Welcome to the Brain Games!');
-  console.log(`${taskGame}\n`);
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}\n`);
+  console.log(`${taskGame}\n`);
   for (let attemptsCount = 0; attemptsCount < answersCount; attemptsCount += 1) {
     const [question, rigthAnswer] = questionAndARigthAnswerGame();
     console.log(`Question: ${question}`);
