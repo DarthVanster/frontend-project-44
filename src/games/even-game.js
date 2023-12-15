f28ac2ed2034d43ac74ee9bbb8a4424b0a58efdb
Alexey <alex14fs@mail.ru>
2023-12-15 20:28:51 +0400

    forVerification

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -1,12 +1,12 @@
-import getRandomNuber from '../randomNumber.js';
+import getRandomNumber from '../randomNumber.js';
 import executeGame from '../execute.js';
 
-const isEven = number => (number % 2 === 0);
+const isEven = (number) => (number % 2 === 0);
 
 const taskGame = 'Answer "yes" if number even otherwise answer "no".';
 
 const getQuestionAndRigthAnswer = () => {
-  const question = getRandomNuber();
+  const question = getRandomNumber(0, 99);
   const rigthAnswer = isEven(question) ? 'yes' : 'no';
   return [`${question}`, rigthAnswer];
 };
